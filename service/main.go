253e3e9f84e6824dda924ec7eab8f7298a58509a
han003 <jagolegion@gmail.com>
2020-08-20 20:21:48 +0200

    Server

--- service/main.go
+++ service/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	gohandlers "github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 const (
@@ -11,13 +18,46 @@ const (
 )
 
 func main() {
-	log.Println("Hello in main")
+	sm := mux.NewRouter()
 
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("HANDLE")
-
-		fmt.Fprintf(rw, "Hello")
+	getR := sm.Methods(http.MethodGet).Subrouter()
+	getR.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(rw, "hello")
 	})
 
-	http.ListenAndServe(port, nil)
+	// CORS
+	ch := gohandlers.CORS(gohandlers.AllowedOrigins([]string{"http://localhost:8080"}))
+
+	// Create a new server
+	s := http.Server{
+		Addr:         ":9090",
+		Handler:      ch(sm),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	// Start the server
+	go func() {
+		log.Println("Starting server on port 9090")
+
+		err := s.ListenAndServe()
+		if err != nil {
+			log.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	// trap sigterm or interupt and gracefully shutdown the server
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	// Block until a signal is received.
+	sig := <-c
+	log.Println("Got signal:", sig)
+
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
 }
