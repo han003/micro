f41a8afebed548c2f54069078cd7278ff382dfdb
han003 <jagolegion@gmail.com>
2020-08-20 21:08:58 +0200

    VUE again

--- eaw-i18n/.eslintrc.js
+++ eaw-i18n/.eslintrc.js
@@ -4,14 +4,26 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/essential',
     '@vue/airbnb',
+    '@vue/typescript/recommended',
   ],
   parserOptions: {
-    parser: 'babel-eslint',
+    ecmaVersion: 2020,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
