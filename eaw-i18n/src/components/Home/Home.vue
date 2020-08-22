49356fa18bf4aeb399087ab41a50742ca281fdda
han003 <jagolegion@gmail.com>
2020-08-22 21:58:08 +0200

    Webpack

--- eaw-i18n/src/components/Home/Home.vue
+++ eaw-i18n/src/components/Home/Home.vue
@@ -45,18 +45,9 @@ export default class Home extends Vue {
 }
 </script>
 <style scoped lang="scss">
+$test: 30px;
+
 h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  margin: $test 0 0;
 }
 </style>
--- eaw-i18n/src/main.ts
+++ eaw-i18n/src/main.ts
@@ -3,9 +3,10 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 
-Vue.config.productionTip = false;
+// Global style
+import '@/assets/styles/main.scss';
 
 new Vue({
   router,
-  render: (h) => h(App),
+  render: h => h(App),
 }).$mount('#app');
